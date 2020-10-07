da28a6639e0e1d5d9a3c93e6d7ad735ebe5c90b9
Alexey Nadtochiy <nadtochiyalex@mail.ru>
2020-10-07 17:24:24 +0300

    Fix readme and templates

--- src/components/cards/MetCard/MetCard.js
+++ src/components/cards/MetCard/MetCard.js
@@ -1,11 +1,6 @@
 'use strict';
 
-/*import {
-    wrapCreateChipsFunc
-} from '../CardChips/Chips.js';*/
-
 export function createMetCard(data) {
-    const tmp = document.createElement('div');
     const monthNames = [
         'января', 
         'февраля', 
@@ -26,23 +21,8 @@ export function createMetCard(data) {
         data.dateStr = date.getDate() + ' ' + monthNames[date.getMonth()] + ' ' + date.getFullYear();
     }
 
-    /*tmp.innerHTML = `
-        <div class="metcard" id="${data.id}metcard">
-            <img src="${data.imgSrc}" class="metimg">
-            <div class="swimblock top">
-                <span>${data.text}</span>
-                <div class="tags"></div>  
-            </div>
-            <h3>${data.title}</h3>
-            <h4>${data.place}</h4>
-            <h4>${dateStr}</h4>
-        </div>
-    `;
-
-    const tags = tmp.getElementsByClassName('tags')[0];
-    data.tags.forEach(wrapCreateChipsFunc(tags));*/
-
-    tmp.innerHTML = window.template(data);
+    const tmp = document.createElement('div');
+    tmp.innerHTML = window.MetCardTemplate(data);
 
     return tmp.firstElementChild;
 }
--- src/components/cards/UserCard/UserCard.js
+++ src/components/cards/UserCard/UserCard.js
@@ -1,38 +1,10 @@
 'use strict';
 
-import {
-    wrapCreateChipsFunc
-} from '../CardChips/Chips.js';
-
 export function createUserCard(data) {
     const tmp = document.createElement('div');
-
     data.job = data.job.replace("\n", "<br/>");
 
-    tmp.innerHTML = `
-        <div class="usercard" id="${data.id}usercard">
-            <img src="${data.imgSrc}" class="overlay">
-            <div class="overlay"></div>
-            <div class="wraper top">
-                <h2>${data.name}</h2>
-                <span>${data.job}</span>
-                
-                <h3>Интересы</h3>
-                <div class="tags"></div>
-                
-                <h3>Навыки</h3>
-                <div class="tags"></div>
-            </div>
-        </div>
-    `;
+    tmp.innerHTML = window.UserCardTemplate(data);
 
-    const interests = tmp.getElementsByClassName('tags')[0];
-    data.interestTags.forEach(wrapCreateChipsFunc(interests));
-
-    const skills = tmp.getElementsByClassName('tags')[1];
-    data.skillTags.forEach(wrapCreateChipsFunc(skills));
-
-    // tmp.innerHTML = window.template(data);
-    
     return tmp.firstElementChild;
 }
