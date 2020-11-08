1139250efdfd95f6121060c23319c94edd25aaba
Alexey Nadtochiy <nadtochiyalex@mail.ru>
2020-11-08 12:07:26 +0300

    Maybe it works

--- src/sw.js
+++ src/sw.js
@@ -1,14 +1,10 @@
-const CACHE_NAME = '7';
-const CACHE_PREFIX = '1234';
+const CACHE_NAME = 'onmeet-v1.0.0';
+const CACHE_PREFIX = 'onmeet';
 
 const CACHE_URLS = [
     'bundle.js',
     'main.css',
     '/',
-    '/meetings',
-    '/people',
-    '/profile',
-    '/meet',
 ];
 
 self.addEventListener('install', function(event) {
@@ -38,19 +34,35 @@ self.addEventListener('activate', function (event) {
     );
 });
 
-self.addEventListener('fetch', function(event) {
-    if (navigator.onLine) {
-		return fetch(event.request);
-    }
-    console.log(event.request);
+self.addEventListener('fetch', function(event) {		
     event.respondWith(
         caches.match(event.request)
             .then(function(response) {
-                console.log(response);
                 // Cache hit - return response
                 if (response) {
                     return response;
                 }
+                return fetch(event.request).then(
+                    function(response) {
+                        // Check if we received a valid response
+                        if(!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+            
+                        // IMPORTANT: Clone the response. A response is a stream
+                        // and because we want the browser to consume the response
+                        // as well as the cache consuming the response, we need
+                        // to clone it so we have two streams.
+                        let responseToCache = response.clone();
+            
+                        caches.open(CACHE_NAME)
+                            .then(function(cache) {
+                                cache.put(event.request, responseToCache);
+                            });
+            
+                        return response;
+                    }
+                );
             })
     );
 });
