9f33b46ba0ecaf7c03dd5182be4b401c4117840a
Alexey Nadtochiy <nadtochiyalex@mail.ru>
2020-11-11 23:29:59 +0300

    Fix api models

--- src/components/cards/MeetCard/MeetCard.js
+++ src/components/cards/MeetCard/MeetCard.js
@@ -3,15 +3,15 @@
 const template = require('./MeetCardTemplate.pug');
 
 export function createMeetCard(data) {
-    let startDate = new Date(data.startDate);
-    let endDate = new Date(data.endDate);
+    let startDate = new Date(data.card.startDate);
+    let endDate = new Date(data.card.endDate);
     let currentDate = Date.now();
 
     // const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     const options = {weekday: 'long', month: 'long', day: 'numeric' };
     data.dateStr = startDate.toLocaleDateString('ru-RU', options);
 
-    if (startDate > currentDate && data.seatsLeft > 0) {
+    if (startDate > currentDate && data.card.seatsLeft > 0) {
         data.status = {
             class: 'meetcard__status status status_green',
             text: 'Регистрация идет',
@@ -31,7 +31,7 @@ export function createMeetCard(data) {
             class: 'meetcard__status status status_yellow',
             text: 'Мероприятие идет',
         };
-    } else if (data.seatsLeft < 10) {
+    } else if (data.card.seatsLeft < 10) {
         data.status = {
             class: 'meetcard__status status status_yellow',
             text: 'Осталось мало мест',
--- src/components/cards/MeetSlides/MeetСarouselItem/MeetSlide.js
+++ src/components/cards/MeetSlides/MeetСarouselItem/MeetSlide.js
@@ -3,7 +3,7 @@
 const template = require('./MeetSlideTemplate.pug');
 
 export function createMeetSlide(data) {
-    let startDate = new Date(data.startDate);
+    let startDate = new Date(data.card.startDate);
     const options = {weekday: 'long', month: 'long', day: 'numeric' };
     data.dateStr = startDate.toLocaleDateString('ru-RU', options);
 
